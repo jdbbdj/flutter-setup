475c9f2d36ddea32caf29cf99414639f1df42925
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2023-03-08 18:40:59 +0800

    Initialize, custom widget designs, custom colors

---
+++ android/app/src/main/kotlin/com/fluttermapp/first_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fluttermapp.first_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+Map<int, Color> color = {
+  50: Color.fromRGBO(4, 131, 184, .1),
+  100: Color.fromRGBO(4, 131, 184, .2),
+  200: Color.fromRGBO(4, 131, 184, .3),
+  300: Color.fromRGBO(4, 131, 184, .4),
+  400: Color.fromRGBO(4, 131, 184, .5),
+  500: Color.fromRGBO(4, 131, 184, .6),
+  600: Color.fromRGBO(4, 131, 184, .7),
+  700: Color.fromRGBO(4, 131, 184, .8),
+  800: Color.fromRGBO(4, 131, 184, .9),
+  900: Color.fromRGBO(4, 131, 184, 1),
+};
+MaterialColor myColor = MaterialColor(0xFF6C63FF, color);
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: myColor),
+      debugShowCheckedModeBanner: false,
+      home: const Login(),
+    );
+  }
+}
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black87,
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 44.0),
+            const Text(
+              "Welcome",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 55,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              "to Map blog",
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 35,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 22.0),
+            SvgPicture.asset(
+              'images/connected-world.svg',
+              width: 180,
+            ),
+            const SizedBox(height: 44.0),
+            ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Color(0xFF6C63FF)),
+                  minimumSize: MaterialStateProperty.all(Size(300, 40)),
+                ),
+                child: const Text("Login")),
+            const SizedBox(height: 11.0),
+            ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.white),
+                  foregroundColor: MaterialStateProperty.all(Color(0xFF6C63FF)),
+                  minimumSize: MaterialStateProperty.all(Size(300, 40)),
+                ),
+                child: const Text("Register"))
+          ],
+        ),
+      ),
+    );
+  }
+}
