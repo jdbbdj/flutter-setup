012a6f81d9d938da3c1a5405d9d7d5a4921446dc
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2023-03-09 10:17:32 +0800

    Fix Regex and add some styling

--- lib/test.dart
+++ lib/test.dart
@@ -16,6 +16,23 @@ class _CityPageState extends State<CityPage> {
   bool isLoading = false;
   final cityName = "City";
   String d = "";
+
+  var mock = {
+    "city": "Manila",
+    "temp": "47",
+    "name": "James",
+    "unit": "123",
+    "history": "Bacon ipsum"
+  };
+
+  String regExpInitiation(String name) {
+    return '"($name)":"((\\"|[^,"])*)"';
+  }
+
+  RegExp printText(String text) {
+    return RegExp(text);
+  }
+
   final TextEditingController _cityController = TextEditingController();
 
   @override
@@ -28,52 +45,59 @@ class _CityPageState extends State<CityPage> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: _cityController,
-                  decoration: InputDecoration(hintText: "Enter $cityName name"),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _cityController,
+                    decoration:
+                        InputDecoration(hintText: "Enter $cityName name"),
+                  ),
                 ),
-              ),
-              ElevatedButton(
-                  onPressed: () async {
-                    setState(() {
-                      isLoading = true;
-                    });
-                    Future.delayed(Duration(milliseconds: 3000), () async {
-                      try {
-                        var response = await http.post(
-                            Uri.parse("https://mybackend/cities"),
-                            body: '{"city_name":${_cityController.text}}',
-                            headers: {
-                              "Authorization":
-                                  "${(await SharedPreferences.getInstance()).getString("TOKEN")}"
-                            });
-                        setState(() {
-                          isLoading = false;
-                        });
-                        d = response.body;
-                      } catch (e) {
-                        setState(() {
-                          isLoading = false;
-                        });
-                        setState(() {
-                          isError = true;
-                        });
-                        d = "";
-                        print("Error: $e");
-                        rethrow;
-                      }
-                    });
-                  },
-                  child: const Text("Generate")),
-            ],
-          ),
-          buildBody(context)
-        ],
+              ],
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  setState(() {
+                    isLoading = true;
+                  });
+                  Future.delayed(Duration(milliseconds: 1500), () async {
+                    try {
+                      var response = await http.post(
+                          Uri.parse("https://mybackend/cities"),
+                          body: '{"city_name":${_cityController.text}}',
+                          headers: {
+                            "Authorization":
+                                "${(await SharedPreferences.getInstance()).getString("TOKEN")}"
+                          });
+                      setState(() {
+                        isLoading = false;
+                      });
+                      d = json.encode(mock);
+                      print(d);
+                    } catch (e) {
+                      setState(() {
+                        isLoading = false;
+                      });
+                      setState(() {
+                        isError = true;
+                      });
+                      d = json.encode(mock);
+                      print(d);
+                      print("Error: $e");
+                      rethrow;
+                    }
+                  });
+                },
+                child: const Text("Generate")),
+            SizedBox(height: 20.0),
+            buildBody(context),
+          ],
+        ),
       ),
     );
   }
@@ -83,44 +107,85 @@ class _CityPageState extends State<CityPage> {
         ? const Center(
             child: CircularProgressIndicator(),
           )
-        : Card(
-            child: Column(
-              children: [
-                Text("MANILA"),
-                RichText(
-                  text: TextSpan(children: [
-                    TextSpan(
-                      text: "HISTORY",
-                    ),
-                    TextSpan(
-                      text: "TEXT",
-                    ),
-                  ]),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 20.0),
-                  height: 180.0,
-                  child: SvgPicture.asset(
-                    'images/departing.svg',
-                    width: 180,
+        : Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 2,
+                    color: Colors.black12,
+                    offset: Offset(2, 2),
+                    spreadRadius: 2,
+                  )
+                ]),
+            child: Card(
+              child: Column(
+                children: [
+                  Text(
+                    "${printText(regExpInitiation('city')).firstMatch(d)?.group(2)}",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold),
                   ),
-                ),
-                Row(
-                  children: [
-                    Column(
-                      children: [Text("Temperature:"), Text("100*")],
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: RichText(
+                      text: TextSpan(children: [
+                        const TextSpan(
+                            text: "HISTORY: ",
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 18)),
+                        TextSpan(
+                            text:
+                                "${printText(regExpInitiation('history')).firstMatch(d)?.group(2)}",
+                            style: const TextStyle(color: Colors.black)),
+                      ]),
                     ),
-                    Container(
-                      height: 20.0,
-                      decoration:
-                          BoxDecoration(border: Border(right: BorderSide())),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 20.0),
+                    height: 180.0,
+                    child: SvgPicture.asset(
+                      'images/departing.svg',
+                      width: 180,
                     ),
-                    Column(
-                      children: [Text("Unit:"), Text("123")],
-                    ),
-                  ],
-                ),
-              ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            const Text("Temperature:"),
+                            Text(
+                                "${printText(regExpInitiation('temp')).firstMatch(d)?.group(2)}")
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 20.0,
+                        decoration: BoxDecoration(boxShadow: [
+                          BoxShadow(
+                            blurRadius: 2,
+                            color: Colors.black12,
+                            offset: Offset(2, 2),
+                            spreadRadius: 2,
+                          )
+                        ]),
+                      ),
+                      Column(
+                        children: [
+                          Text("Unit:"),
+                          Text(
+                              "${printText(regExpInitiation('unit')).firstMatch(d)?.group(2)}")
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           );
   }
